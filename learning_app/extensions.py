9ebe601012c215ec47881889c59e76dc617b66b7
EmmanuelDanso1 <kwekudanso21@gmail.com>
2025-12-06 11:51:33 +0000

    encoding character using quote_plus

--- learning_app/extensions.py
+++ learning_app/extensions.py
@@ -6,9 +6,11 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from urllib.parse import quote_plus
 
 # Load Redis password from environment variable
-REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+# using quote_plus for encoding the "@" character
+REDIS_PASSWORD = quote_plus(os.getenv("REDIS_PASSWORD"))
 
 limiter = Limiter(
     key_func=get_remote_address,
