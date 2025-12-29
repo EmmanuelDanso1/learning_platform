4f7053b694c650a31a1b1a4e695bdd89174b046f
EmmanuelDanso1 <kwekudanso21@gmail.com>
2025-12-29 20:40:05 +0000

    new update

--- learning_app/realmind/utils/util.py
+++ learning_app/realmind/utils/util.py
@@ -13,9 +13,8 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4', 'mov', 'avi'}
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
 FLIERS_FOLDER = os.path.join(BASE_DIR, "realmind", "static", "fliers")
-
 # Helpers
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
