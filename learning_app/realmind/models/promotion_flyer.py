887c346337bc560b18abfbb1805c4edea025e22d
EmmanuelDanso1 <kwekudanso21@gmail.com>
2025-12-29 19:00:50 +0000

    new update

--- learning_app/realmind/models/promotion_flyer.py
+++ learning_app/realmind/models/promotion_flyer.py
@@ -1,6 +1,9 @@
 from learning_app.extensions import db
+from datetime import datetime
 
 class PromotionFlier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150), nullable=False)
     image_filename = db.Column(db.String(200), nullable=False)
+    external_id = db.Column(db.Integer, nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
