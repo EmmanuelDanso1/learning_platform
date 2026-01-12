9b80ca914da2b0f9253fb86c4b8bbeda6e87ce61
EmmanuelDanso1 <kwekudanso21@gmail.com>
2026-01-12 15:29:37 +0000

    update cert with nullable true

--- learning_app/realmind/models/application.py
+++ learning_app/realmind/models/application.py
@@ -9,7 +9,7 @@ class Application(db.Model):
     date_applied = db.Column(db.DateTime, default=datetime.utcnow)
     status = db.Column(db.String(20), default='under review')
     cv = db.Column(db.String(150), nullable=False)
-    certificate = db.Column(db.String(150), nullable=False)
+    certificate = db.Column(db.String(150), nullable=True)
     cover_letter = db.Column(db.String(150), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     job_id = db.Column(db.Integer, db.ForeignKey('job_post.id'), nullable=False)
